a725988bbfa2e688436f07e67e1f295d24989ded
FeryleneValentin <FeryleneValentin@SMU-0047>
2025-03-25 18:14:05 +0800

    names, petnamecontroller

--- PetNameGenerator.API/Controllers/PetNameController.cs
+++ PetNameGenerator.API/Controllers/PetNameController.cs
@@ -2,11 +2,76 @@
 
 namespace PetNameGenerator.API.Controllers
 {
-    public class PetNameController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class PetNameController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpPost("/generate")]
+        public IActionResult Post([FromQuery] string animalType, [FromQuery] bool? hasLastName)
         {
-            return View();
+
+            if (string.IsNullOrEmpty(animalType))
+            {
+                return BadRequest(new { error = "The 'animalType' field is required." });
+            }
+
+            animalType = animalType.ToLower();
+
+            if (animalType != "dog" && animalType != "cat" && animalType != "bird")
+            {
+                return BadRequest(new { error = "Invalid animal type. Allowed values: dog, cat, bird." });
+            }
+
+            if (hasLastName.HasValue && !hasLastName.HasValue)
+            {
+                return BadRequest(new { error = "The 'hasLastName' field must be a boolean (true or false)." });
+            }
+
+
+            bool useLastName = hasLastName ?? false;
+
+            string firstName = "";
+            string lastName = "";
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, 5);
+
+
+            if (animalType == "dog")
+            {
+                firstName = Constants.Names.Dog.First[randomIndex];
+                if (useLastName)
+                {
+                    lastName = Constants.Names.Dog.Last[randomIndex];
+                }
+            }
+            else if (animalType == "cat")
+            {
+                firstName = Constants.Names.Cat.First[randomIndex];
+                if (useLastName)
+                {
+                    lastName = Constants.Names.Cat.Last[randomIndex];
+                }
+            }
+            else if (animalType == "bird")
+            {
+                firstName = Constants.Names.Bird.First[randomIndex];
+                if (useLastName)
+                {
+                    lastName = Constants.Names.Bird.Last[randomIndex];
+                }
+            }
+
+
+            if (useLastName)
+            {
+                return Ok(new { firstName, lastName });
+            }
+            else
+            {
+                return Ok(new { firstName });
+            }
         }
     }
 }
+    
